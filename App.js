1e28b7ddb3f17541d144045d4eaecaa8b2b14349
rudy <doomdexter07@gmail.com>
2022-04-19 19:49:48 +0530

    created 3 new Screen Listings,Account,Message

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 
+import AccountScreen from './app/screens/AccountScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import MessagesScreen from './app/screens/MessageScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
   return (
-   <ListingDetailsScreen/>
+   <ListingsScreen/>
   );
 }
 
