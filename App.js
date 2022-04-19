1c195259328b08ae3f3a532f60aea67953d73607
rudy <doomdexter07@gmail.com>
2022-04-19 22:35:22 +0530

    created Input component and categories screen

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
+import React, { useState } from "react";
 
-import AccountScreen from './app/screens/AccountScreen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import MessagesScreen from './app/screens/MessageScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
+import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
+
   return (
-   <ListingsScreen/>
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
   );
 }
-
-
