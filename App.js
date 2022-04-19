4e465747343d0392dc6cfb8bee3b3dfaa7d0d86a
rudy <doomdexter07@gmail.com>
2022-04-19 18:11:29 +0530

    created viewImage Screen

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-   <WelcomeScreen/>
+   <ViewImageScreen></ViewImageScreen>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
