a97f8f1bea3aea4ce1564fa308e0f4b6398f4600
rudy <doomdexter07@gmail.com>
2022-04-19 18:01:44 +0530

    created WelcomScreen

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Mother-Fucker</Text>
-      <StatusBar style="auto" />
-    </View>
+   <WelcomeScreen/>
   );
 }
 
