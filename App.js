f7b58d1fe1080cb455a06e3a59c3485e5c6925be
rudy <doomdexter07@gmail.com>
2022-04-20 12:18:29 +0530

    add image Features to the ListingEditScreen

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
 import ImageInput from "./app/components/ImageInput";
 import Screen from "./app/components/Screen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
   return (
-    <Screen>
-      <ImageInput
-        imageUri={imageUri}
-        onChangeImage={(uri) => setImageUri(uri)}
-      />
-    </Screen>
+    <ListingEditScreen/>
   );
 }
