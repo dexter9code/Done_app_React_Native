db0c00767bbc434d4060c54bb0946ebab86d61e6
rudy <doomdexter07@gmail.com>
2022-04-20 11:47:14 +0530

    created ImageInput component

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import React from "react";
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import React, { useState } from "react";
+import ImageInput from "./app/components/ImageInput";
+import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
   return (
-    <ListingEditScreen/>
+    <Screen>
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
+    </Screen>
   );
 }
---
+++ app/components/ImageInput.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import {
+  StyleSheet,
+  Alert,
+  TouchableWithoutFeedback,
+  View,
+  Image,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+
+function ImageInput({ imageUri, onChangeImage }) {
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    const result = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!result.granted) alert("You Need To Enable Permission");
+  };
+
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert("Delete", "Are You Sure You Want To Delete", [
+        { text: "Yes", onPress: () => onChangeImage(null) },
+        { text: "No" },
+      ]);
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.cancelled) onChangeImage(result.uri);
+    } catch (error) {
+      console.log("Error While Acessing the Library", error);
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons color={colors.medium} name="camera" size={40} />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.light,
+    borderRadius: 15,
+    height: 100,
+    justifyContent: "center",
+    overflow: "hidden",
+    width: 100,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
+
+export default ImageInput;
