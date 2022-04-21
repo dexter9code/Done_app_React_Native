f01525915daaa1b42f2e825047172719b153360a
rudy <doomdexter07@gmail.com>
2022-04-21 14:22:28 +0530

    created FeedNavigator for listing and listingEdit screen

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -4,15 +4,16 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ListingEditScreen from "./../screens/ListingEditScreen";
 import ListingsScreen from "./../screens/ListingsScreen";
 import AccountScreen from "./../screens/AccountScreen";
+import FeedNavigator from "./FeedNavigator";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Listings" component={ListingsScreen} />
+    <Tab.Screen name="Feed" component={FeedNavigator} />
     <Tab.Screen name="ListingsEdit" component={ListingEditScreen} />
     <Tab.Screen name="Account" component={AccountScreen} />
   </Tab.Navigator>
-) 
+);
 
 export default AppNavigator;
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -5,17 +5,18 @@ import AppText from "../components/AppText";
 import colors from "../config/colors";
 import ListItem from "./../components/lists/ListItem";
 
-function ListingDetailsScreen(props) {
+function ListingDetailsScreen({ route }) {
+  const listing = route.params;
   return (
     <View>
-      <Image style={styles.image} source={require("../assets/redJacket.jpg")} />
+      <Image style={styles.image} source={listing.image} />
       <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>Red jacket for sale</AppText>
-        <AppText style={styles.price}>$100</AppText>
+        <AppText style={styles.title}>{listing.title}</AppText>
+        <AppText style={styles.price}>${listing.price}</AppText>
         <View style={styles.userContainer}>
           <ListItem
-            image={require("../assets/faceImage.jpg")}
-            title="Mosh Hamedani"
+            image={require("../assets/face.jpg")}
+            title="Ranveer Singh"
             subTitle="5 Listings"
           />
         </View>
