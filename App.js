6556ceeab85a1f7037bc2011218d8fb0d12292dd
rudy <doomdexter07@gmail.com>
2022-04-21 11:55:04 +0530

    install tab navigation package/library

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import React, { useState } from "react";
-import ImageInput from "./app/components/ImageInput";
+import { Text, Button } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
 import Screen from "./app/components/Screen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Just Tweet</Text>
+    <Button
+      title="Tweet-Details"
+      onPress={() => navigation.navigate("TweetsDetails")}
+    />
+  </Screen>
+);
+const TweetsDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetsDetails" component={TweetsDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
-    <ListingEditScreen/>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
