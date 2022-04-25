5fc5bef05632888cdc36504e3800ee478acb2b51
rudy <doomdexter07@gmail.com>
2022-04-25 23:40:44 +0530

    add offline sign

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./app/navigation/AppNavigator";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
+import OfflineNotice from './app/components/OffineNotice';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
