e1c39105f06c5bbe3d31294945ed50948ac4000d
rudy <doomdexter07@gmail.com>
2022-04-26 17:36:29 +0530

    keep state unchanged after the refresh and add custom hook

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,12 +1,11 @@
-import React, { useContext } from "react";
+import React from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 
 import Screen from "../components/Screen";
 import { ListItem, ListItemSeparatorComponent } from "../components/lists";
 import Icon from "../components/Icon";
 import colors from "../config/colors";
-import AuthContext from "./../auth/context";
-import authStorage from "../auth/storage";
+import useAuth from "../auth/useAuth";
 
 const menuItems = [
   {
@@ -27,12 +26,8 @@ const menuItems = [
 ];
 
 function AccountScreen({ navigation }) {
-  const { user, setUser } = useContext(AuthContext);
+  const { user, logOut } = useAuth();
 
-  const handleLogout = () => {
-    setUser(null);
-    authStorage.removeToken();
-  };
   return (
     <Screen style={styles.screen}>
       <View style={styles.container}>
@@ -63,7 +58,7 @@ function AccountScreen({ navigation }) {
       </View>
       <ListItem
         title="Log Out"
-        onPress={handleLogout}
+        onPress={() => logOut()}
         IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
       />
     </Screen>
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,9 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { StyleSheet, Image } from "react-native";
 import * as Yup from "yup";
-import jwtDecode from "jwt-decode";
 
 import authApi from "../api/auth";
-import AuthContext from "./../auth/context";
-import authStorage from "../auth/storage";
+import useAuth from "../auth/useAuth";
 import Screen from "../components/Screen";
 import {
   AppForm,
@@ -20,17 +18,14 @@ const validationSchema = Yup.object().shape({
 });
 
 function LoginScreen(props) {
-  const authContext = useContext(AuthContext);
+  const { logIn } = useAuth();
   const [loginFailed, setLoginFailed] = useState();
 
   const handleSubmit = async ({ email, password }) => {
     const result = await authApi.login(email, password);
     if (!result.ok) return setLoginFailed(true);
     setLoginFailed(false);
-    const user = jwtDecode(result.data);
-    console.log(user);
-    authContext.setUser(user);
-    authStorage.storeToken(result.data);
+    logIn(result.data);
   };
   return (
     <Screen style={styles.container}>
